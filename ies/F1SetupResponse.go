01470ec66cd16fc516d4e3c1aba2ab6414ffa6e4
TRAN NGUYEN HA LAN <halannevergiveup@gmail.com>
2025-11-19 21:46:47 +0700

    update readbool()

--- ies/F1SetupResponse.go
+++ ies/F1SetupResponse.go
@@ -108,7 +108,7 @@ func (msg *F1SetupResponse) Decode(wire []byte) (err error, diagList []Criticali
 		}
 	}()
 	r := aper.NewReader(bytes.NewReader(wire))
-	r.ReadBool()
+	//r.ReadBool()
 	decoder := F1SetupResponseDecoder{
 		msg:  msg,
 		list: make(map[aper.Integer]*F1apMessageIE),
